c2f0b9a7bd127a6981dcc8ca2155492e2e8bc16e
Cesar Santos <csar.santos18@gmail.com>
2025-01-08 10:12:12 -0500

    segunda correção  da atividade

--- src/components/PostComments/index.tsx
+++ src/components/PostComments/index.tsx
@@ -3,45 +3,45 @@ import Comment from "../../models/Comment";
 import styles from "./PostComments.module.css";
 
 const PostComments = () => {
-  const [comments, setComments] = useState<Comment[]>([]);
-  const [tempComment, setTempComment] = useState("");
+const [comments, setComments] = useState<Comment[]>([]);
+const [tempComment, setTempComment] = useState("");
 
-  function handleAddComment(event: FormEvent<HTMLFormElement>) {
+function handleAddComment(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const newComment = new Comment(comments.length, tempComment);
     setTempComment("");
     setComments([...comments, newComment]);
-  }
+}
 
-  return (
+return (
     <div>
-      <ul className={styles["post-comments"]} data-testid="post-comments">
+    <ul className={styles["post-comments"]} data-testid="post-comments">
         {comments.map(({ comment, id }) => (
-          <li
+        <li
             className={styles["post-comment"]}
             key={id}
             data-testid="post-comment"
-          >
+        >
             <p className={styles["post-comment-content"]}>{comment}</p>
-          </li>
+        </li>
         ))}
-      </ul>
-      <form
+    </ul>
+    <form
         onSubmit={handleAddComment}
         className={styles["post-comments-form"]}
-      >
+    >
         <textarea
-          value={tempComment}
-          onChange={(e) => setTempComment(e.target.value)}
-          required
-          className={styles["post-comments-form-textarea"]}
+        value={tempComment}
+        onChange={(e) => setTempComment(e.target.value)}
+        required
+        className={styles["post-comments-form-textarea"]}
         />
         <button type="submit" className={styles["post-comments-form-button"]}>
-          Comentar
+        Comentar
         </button>
-      </form>
+    </form>
     </div>
-  );
+);
 };
 
 export default PostComments;
